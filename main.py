bd5857668eb73ba71388aeaca3af69eaf42bb66f
AYM1104 <69380356+AYM1104@users.noreply.github.com>
2025-07-24 04:42:56 +0900

    Update main.py

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ app = FastAPI()
 # CORS設定
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Next.jsのデフォルトポート
+    allow_origins=["https://salesaiagentbackend-production.up.railway.app"],  # Next.jsのデフォルトポート
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
